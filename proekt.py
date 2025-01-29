88950050f4aeb3814f00013f364792fce1fd5d80
Timur705 <isakov2003@gmail.com>
2025-01-29 16:06:41 +0300

    aaa

--- proekt.py
+++ proekt.py
@@ -6,7 +6,7 @@ HugginFace_Token=os.getenv("HugginFace_Token")
 
 client = OpenAI(
 	base_url="https://api-inference.huggingface.co/v1/",
-	api_key=HugginFace_Token
+	api_key="hf_pZyOubyUfCEyFdIoOnQkmKBITIoCFeoyL"
 )
 
 zapros=st.text_input("Введите запрос", "Напиши сказку про кота")
