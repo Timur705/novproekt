c4d1aa96e8f077badc351eed4d9fc8c983692b3f
Timur705 <isakov2003@gmail.com>
2025-01-29 16:18:44 +0300

    aaa

--- proekt.py
+++ proekt.py
@@ -1,10 +1,14 @@
 import streamlit as st
 from openai import OpenAI
+#import os
+
+#HugginFace_Token = os.getenv("HugginFace_Token")
+
 
 client = OpenAI(
 	base_url="https://api-inference.huggingface.co/v1/",
-	#api_key="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"
-    api_key="HugginFace_Token" 
+	api_key="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"
+    #api_key=HugginFace_Token 
 )
 
 zapros=st.text_input("Введите запрос", "Напиши сказку про кота")
